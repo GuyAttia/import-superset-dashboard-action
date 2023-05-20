3ff13eea6cb1b28e5f07c5d05e1804a81fa155c5
Guy Attia <31534531+GuyAttia@users.noreply.github.com>
2023-05-20 23:12:26 +0300

    Update script to use DB passwords if needed

--- import_dashboard.py
+++ import_dashboard.py
@@ -3,6 +3,7 @@ import requests.exceptions
 import requests_oauthlib
 import urllib.parse
 from os import path, getenv
+import json as js
 
 def login_superset():
     """
@@ -10,25 +11,30 @@ def login_superset():
 
     Returns: Login token (Bearer) and Session object
     """
-    print('Start Superset authentication')
-    # Login
-    login_endpoint = urllib.parse.urljoin(URL_BASE, '/api/v1/security/login')
-    response = requests.post(
-        login_endpoint,
-        json={
-            "username": getenv('SUPERSET_USERNAME'),
-            "password": getenv('SUPERSET_PASSWORD'),
-            "provider": 'db',
-            "refresh": "true",
-        },
-    )
+    try:
+        print('Start Superset authentication')
+        # Login
+        login_endpoint = urllib.parse.urljoin(URL_BASE, '/api/v1/security/login')
+        response = requests.post(
+            login_endpoint,
+            json={
+                "username": getenv('SUPERSET_USERNAME'),
+                "password": getenv('SUPERSET_PASSWORD'),
+                "provider": 'db',
+                "refresh": "true",
+            },
+        )
 
-    login_token = response.json()
+        login_token = response.json()
 
-    # Create session
-    session = requests_oauthlib.OAuth2Session(token=login_token)
-    print('Session created successfully')
+        # Create session
+        session = requests_oauthlib.OAuth2Session(token=login_token)
+        print('Session created successfully')
+    except Exception as e:
+        print(f'Failed login Superset: {e}')
+        raise(e)
     return login_token, session
+
 
 def get_csrf_token(session):
     """
@@ -39,17 +45,40 @@ def get_csrf_token(session):
     
     Returns: CSRF token
     """
-    print('Start request to retrieve CSRF token')
-    # Get CSRF Token
-    csrf_endpoint = urllib.parse.urljoin(URL_BASE, '/api/v1/security/csrf_token/')
-    headers={"Referer": csrf_endpoint}
-    csrf_response = session.get(
-        url=csrf_endpoint,
-        headers=headers
-    )
-    csrf_token = csrf_response.json().get("result")
-    print('CSRF token received succesfully')
+    try:
+        print('Start request to retrieve CSRF token')
+        # Get CSRF Token
+        csrf_endpoint = urllib.parse.urljoin(URL_BASE, '/api/v1/security/csrf_token/')
+        headers={"Referer": csrf_endpoint}
+        csrf_response = session.get(
+            url=csrf_endpoint,
+            headers=headers
+        )
+        csrf_token = csrf_response.json().get("result")
+        print('CSRF token received succesfully')
+    except Exception as e:
+        print(f'Failed creating the CSRF token: {e}')
+        raise(e)
     return csrf_token
+
+
+def create_passwords_string() -> str:
+    """
+    Generate the passwords string for the payload
+
+    Returns: String mappign the DBs and their passwords
+    """
+    try:
+        DBS_PASSWORDS = getenv('DBS_PASSWORDS', {})
+        pass_map = js.loads(DBS_PASSWORDS)
+        pass_dict = {f"databases/{k}.yaml": v for k, v in pass_map.items()}
+
+        passwords_string = str(pass_dict)
+        return passwords_string
+    except Exception as e:
+        print(f'Failed creating the password string: {e}')
+        raise(e)
+    
 
 def import_dashboard(session, login_token, csrf_token):
     """
@@ -60,36 +89,47 @@ def import_dashboard(session, login_token, csrf_token):
         login_token -- Bearer token for the API request
         csrf_token -- CSRF token for the API request
     """
-    container_dashboard_path = path.join('/', 'github', 'workspace', DASHBOARD_FILE_PATH)
-    print(f'Start importing the dashboard from {container_dashboard_path}')
-    # Import dashboard
-    import_endpoint = urllib.parse.urljoin(URL_BASE, '/api/v1/dashboard/import/')
+    try:
+        container_dashboard_path = path.join('/', 'github', 'workspace', DASHBOARD_FILE_PATH)
+        print(f'Start importing the dashboard from {container_dashboard_path}')
+        # Import dashboard
+        import_endpoint = urllib.parse.urljoin(URL_BASE, '/api/v1/dashboard/import/')
 
-    headers = {
-        'accept': 'application/json',
-        'Authorization': f'Bearer {login_token}',
-        'X-CSRFToken': csrf_token,
-        'Referer': import_endpoint
-    }
+        headers = {
+            'accept': 'application/json',
+            'Authorization': f'Bearer {login_token}',
+            'X-CSRFToken': csrf_token,
+            'Referer': import_endpoint
+        }
 
-    files = { 
-    'formData': (
-        container_dashboard_path, 
-        open(container_dashboard_path, 'rb'), 
-        'application/json'
-        )
-    }
+        files = { 
+        'formData': (
+            container_dashboard_path, 
+            open(container_dashboard_path, 'rb'), 
+            'application/json'
+            )
+        }
 
-    payload = {"overwrite":'true'}
+        OVERWRITE = getenv('OVERWRITE', True)
+        overwrite_payload = str(OVERWRITE).lower()
+        payload = {"overwrite": overwrite_payload}
 
-    response = session.post(
-                url=import_endpoint, 
-                headers=headers,
-                files=files,
-                data=payload
-        )
-    assert response.status_code == 200, f'Error in importing the dashboard\n{response}'
-    print(f'Dashboard imported succesfully from {container_dashboard_path}')
+        # DBS Passwords
+        passwords_string = create_passwords_string()
+        if len(passwords_string) > 2:   # Only if there is at least 1 DB password
+            payload["passwords"] = passwords_string
+
+        response = session.post(
+                    url=import_endpoint, 
+                    headers=headers,
+                    files=files,
+                    data=payload
+            )
+        assert response.status_code == 200, f'Error in importing the dashboard\n{response}'
+        print(f'Dashboard imported succesfully from {container_dashboard_path}')
+    except Exception as e:
+        print(f'Failed importing the dashboard: {e}')
+        raise(e)
 
 def main():
     login_token, session = login_superset()
